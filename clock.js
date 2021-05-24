f53ea9d5e97a70bd55a9970b6b2c34709b035864
Jiseon <js.coding.place@gmail.com>
2021-05-24 16:56:11 +0900

    JS-clock_js

--- clock.js
+++ clock.js
@@ -6,10 +6,19 @@ const clockContainer = document.querySelector(".clock_js"),
 
 
 
+function getTime() {
+    const date = new Date();
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+   
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0️${minutes}` : minutes}:${seconds < 10 ? `0️${seconds}` : seconds}`
+}
+
 
 
 function init() {
-
+    setInterval(getTime, 0);
 }
 
 init();
