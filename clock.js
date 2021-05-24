83efc5271b8bda237bcaae8317e714bb4fd9ac7b
Jiseon <js.coding.place@gmail.com>
2021-05-24 17:25:44 +0900

    CSS-#clock

--- clock.js
+++ clock.js
@@ -11,7 +11,7 @@ function getTime() {
     let hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    const ampm = hours >= 12 ? 'pm' : 'am';
+    const ampm = hours >= 12 ? 'PM' : 'AM';
 
     hours = hours %= 12;
     hours = hours ? hours : 12;
