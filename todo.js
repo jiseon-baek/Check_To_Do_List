4390ebf759696bc4398b039f9de84c5ff4be953e
Jiseon <js.coding.place@gmail.com>
2021-05-25 17:47:38 +0900

    JS-todo-more conciseness

--- todo.js
+++ todo.js
@@ -14,15 +14,16 @@ const toDos = [];
 function showToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "⭕️"
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText = "⭕️"
     span.innerText = text;
     li.appendChild(span);
     li.appendChild(delBtn);
     toDoList.appendChild(li);
     const toDoObj = {
         text: text,
-        id: toDos.length + 1
+        id: newId
     }
     toDos.push(toDoObj);
 }
