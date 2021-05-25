8c3726e7bd87db57d3e1ab8647b05544a4509e65
Jiseon <js.coding.place@gmail.com>
2021-05-25 16:09:30 +0900

    JS-toDo-beginning set

--- todo.js
+++ todo.js
@@ -1,2 +1,14 @@
 'use strict'
 
+
+const toDoForm = document.querySelector(".toDoForm_js"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".toDoList_js");
+
+
+function init() {
+
+}
+
+init();
+
