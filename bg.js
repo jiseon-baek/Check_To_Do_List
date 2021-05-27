389c65539e3a38ea52d4c47222cb0ba25ec71936
Jiseon <js.coding.place@gmail.com>
2021-05-27 13:31:48 +0900

    JS-bg-fuction paintImage to show backgroundimages

--- bg.js
+++ bg.js
@@ -5,6 +5,13 @@ const body = document.querySelector('body');
 
 const Img_NB = 10;
 
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpg`;
+    body.appendChild(image);
+
+}
+
 function genRandom() {
     const number = Math.floor(Math.random() * Img_NB);
     return number;
@@ -13,7 +20,8 @@ function genRandom() {
 
 function init() {
     const randomNumber = genRandom();
-    
+    paintImage(randomNumber);
+
 }
 
 init();
