a32440f217bb1a4c4307fcd5781a06104c8ad087
Jiseon <js.coding.place@gmail.com>
2021-05-27 13:19:47 +0900

    JS-bg-create genRandom to choose image randomly

--- bg.js
+++ bg.js
@@ -3,9 +3,17 @@
 
 const body = document.querySelector('body');
 
+const Img_NB = 10;
+
+function genRandom() {
+    const number = Math.floor(Math.random() * Img_NB);
+    return number;
+};
+
 
 function init() {
-
+    const randomNumber = genRandom();
+    
 }
 
 init();
